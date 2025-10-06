9f34ecc23788483a6f731f8315ad42060a634dac
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-06 15:22:12 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/* 
+   Calcolo del prezzo del biglietto del treno:
+    - Chiedere i km da percorrere
+    - Chiedere l'età del passeggero
+    - Calcolare il prezzo base (0.21 € al km)
+    - Applicare eventuali sconti:
+        - 20% per i minorenni (<18)
+        - 40% per gli over 65 (>=65)
+    - Mostrare il prezzo finale con due decimali
+*/
+
+
